9a668db5bfd87116b6cc8f7370a1a41c6214c02a
psrirajan <psrirajan@gmail.com>
2019-04-11 14:37:07 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using ClassLibrary1;
+
+namespace Clearmeasure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   Console.WriteLine("Hello World!");
+               for (int i=1;i<=100;i++)
+               {
+                   //check the no divisible by 3 then print Fizz or check the no divisible by 5 then print Buzz
+                   if ((i % 3 ) == 0)
+                       Console.WriteLine("Fizz");
+                   if ((i % 5) == 0)
+                       Console.WriteLine("Buzz");
+
+                   if ((i % 3 != 0) && (i % 5 != 0))
+                       Console.WriteLine(i);
+
+
+
+               }
+               Console.ReadKey();*/
+
+            ClassLibrary1.Class1 c=new ClassLibrary1.Class1();
+            c.Main();
+        }
+    }
+}
